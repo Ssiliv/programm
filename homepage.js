4af87615c17160d4848886c355fc481c7f106a46
Ssiliv <64543057+Ssiliv@users.noreply.github.com>
2020-04-29 18:39:06 +0300

    Create homepage.js

---
+++ homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+class HomePage extends React.Component{
+
+    render(){
+        return(
+            <Button>Кнопка!</Button>
+        );
+    }
+}
+
+
+export default HomePage;
